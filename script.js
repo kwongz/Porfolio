941fc1dcacfc4b9779dbef41711de827cfb76478
kwongz <kyle.wong917@gmail.com>
2020-12-17 15:52:30 -0500

    removed console.logs

--- script.js
+++ script.js
@@ -19,21 +19,18 @@ setInterval(function() {
     } else {
         i-- //when direction is false delete the prvious letter
     }
-    console.log("i", i);
     if (i >= typingText[thisWordIndex].length) {
         let pause = setInterval(function(){
             direction = false
             clearInterval(pause)
-        },500)
+        },700)
     }  if(i <= 0) {
         direction = true
         thisWordIndex = thisWordIndex + 1;
         if (thisWordIndex >= typingText.length){
             thisWordIndex = 0;
-            console.log(thisWordIndex);
         }
     }
-    console.log(direction)
     let text = typingText[thisWordIndex].slice(0, i);
     document.getElementById("typeWriter").innerHTML = text;
 }, 150);
